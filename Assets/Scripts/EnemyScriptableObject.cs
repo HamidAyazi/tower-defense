8d4f46f25b3486e1f87c4e7b61d50544abb34712
Mohamad Amin Javan <aminjavan22478@gmail.com>
2023-07-25 11:39:49 +0330

    add comments to scriptable objects

--- Assets/Scripts/EnemyScriptableObject.cs
+++ Assets/Scripts/EnemyScriptableObject.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="Enemy", menuName ="ScriptableObjects/Enemy")]
 public class EnemyScriptableObject : ScriptableObject
 {
-    
+    /*-------- Game Logic Attributes --------*/
 
+    // Base health point of each "Enemy" unit. Unit dies after "HealthPoint" reaches value '0'. Scales with "WaveNumber".
+    public int BaseHealthPoint;
+    // Base damage of each "Enemy" unit. If unit reaches "Goal", the "HealthPoint" of "Goal" reduces by it's "Damage". Scales with "WaveNumber".
+    public int BaseDamage;
+    // Movement speed of each "Enemy" unit. The "Speed" shows how fast each unit can move.
+    public int MovementSpeed;
+    //public int BaseArmor;
+
+    /*-------- Unity Attributes --------*/
+
+    // "EnemyPrefab" is used to store each "Enemy" unit's "Prefab" inside "Unity".
+    public Transform EnemyPrefab;
 }
