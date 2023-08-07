f7b930df31ced596c67ae4710ff2dfacbe432ecd
Mohamad Amin Javan <aminjavan22478@gmail.com>
2023-08-07 17:00:04 +0330

    reset prefabs

--- Assets/Scripts/MonoBehaviours/Towers/HeadRotation.cs
+++ Assets/Scripts/MonoBehaviours/Towers/HeadRotation.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class HeadRotation : MonoBehaviour
@@ -10,28 +6,18 @@ public class HeadRotation : MonoBehaviour
     private Vector3 Direction;
     private float RotationSpeed = 60f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         TargetEnemy = transform.parent.GetComponent<AttackerTower>().TargetEnemy;
         if (TargetEnemy != null)
         {
-            Direction = (TargetEnemy.transform.position - transform.position).normalized;
-            Quaternion Rotation = Quaternion.Euler(0f, 0f, GetAngleFromVector(Direction));
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Rotation, RotationSpeed * Time.deltaTime);
+            Vector3 Direction = TargetEnemy.transform.position - transform.position;
+            float Angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.Euler(0f, 0f, Angle - 90f); // Offset by -90 degrees
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, RotationSpeed * Time.deltaTime);
         }
 
-    }
-    private float GetAngleFromVector(Vector3 Vector)
-    {
-        float Radians = Mathf.Atan2(Vector.y, Vector.x);
-        float Degrees = Radians * Mathf.Rad2Deg;
-        return Degrees;
     }
 }
