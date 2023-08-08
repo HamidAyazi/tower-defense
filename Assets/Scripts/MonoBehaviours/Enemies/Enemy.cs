b9ea690600d5c8e9faedda9918035f14cabce12f
Mohamad Amin Javan <aminjavan22478@gmail.com>
2023-08-08 11:44:35 +0330

    Goal Health System

--- Assets/Scripts/MonoBehaviours/Enemies/Enemy.cs
+++ Assets/Scripts/MonoBehaviours/Enemies/Enemy.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public EnemyScriptableObject EnemyType;
+    private int Damage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Here calculates "Damage" based on "Level"
+        Damage = EnemyType.BaseDamage;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Goal goal = collision.GetComponent<Goal>();
+        if (goal != null)
+        {
+            // Hit an Enemy!
+            goal.Damage(Damage);
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/MonoBehaviours/Goal.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField] private GoalScriptableObject GoalSO;
+    private int MaximumHealthPoint;
+    private int HealthPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Calculation based on levels goes here
+        MaximumHealthPoint = GoalSO.BaseHealthPoint;
+        HealthPoint = MaximumHealthPoint;
+    }
+
+    public void Damage(int DamageReceived)
+    {
+        HealthPoint -= DamageReceived;
+        HealthPoint = Mathf.Clamp(HealthPoint, 0, MaximumHealthPoint);
+        Debug.Log("goal hp : " + HealthPoint);
+        //OnDamaged?.Invoke(this, EventArgs.Empty);
+        if (IsDead())
+        {
+            //OnDied?.Invoke(this, EventArgs.Empty);
+            Destroy(gameObject);
+            Debug.Log("goal died");
+        }
+
+    }
+    public int GetHealthPoint()
+    {
+        return HealthPoint;
+    }
+    public float GetHealthPointNormalized()
+    {
+        return (float)HealthPoint / MaximumHealthPoint;
+    }
+    public bool IsDead()
+    {
+        return HealthPoint == 0;
+    }
+}
