07cfbb62d3f080c5bdf24989272b2bfa5afd834e
Hamid Ayazi <rubiker.hamid@gmail.com>
2023-08-19 13:12:35 +0330

    updated ui click handler

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Tile : MonoBehaviour
+public class Tile : MonoBehaviour, IPointerClickHandler
 {
     public GameObject Tower;
     public GameObject TowerShop;
 
-    void OnMouseDown(){
+    public void OnPointerClick(PointerEventData eventData){
+        Debug.Log("Clicked");
         TileManager.Instance.SelectedTile = this;
         if (Tower != null){
             Debug.Log(Tower);
@@ -17,6 +19,18 @@ public class Tile : MonoBehaviour
             OpenShopWindow();
         }
     }
+
+    // void OnMouseDown(){
+    //     Debug.Log("Clicked");
+    //     TileManager.Instance.SelectedTile = this;
+    //     if (Tower != null){
+    //         Debug.Log(Tower);
+    //         TowerShop.SetActive(false);
+    //         return;
+    //     } else {
+    //         OpenShopWindow();
+    //     }
+    // }
 
     void OpenShopWindow(){
         TowerShop.SetActive(true);
