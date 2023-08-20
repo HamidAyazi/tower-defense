5dbfd911d20e54d72931bf8b9ede0774cbe1f6ed
Hamid Ayazi <rubiker.hamid@gmail.com>
2023-08-20 19:13:36 +0330

    cached get component

--- Assets/Scripts/MonoBehaviour/Tile.cs
+++ Assets/Scripts/MonoBehaviour/Tile.cs
@@ -6,18 +6,24 @@ using UnityEngine.EventSystems;
 public class Tile : MonoBehaviour, IPointerClickHandler
 {
     private GameObject Tower;
+    private Shop shop;
     [SerializeField] private GameObject TowerShop;
+
+
+    void Start(){
+        shop = TowerShop.GetComponent<Shop>();
+    }
 
     public void OnPointerClick(PointerEventData eventData){
         TileManager.Instance.SelectedTile = this;
 
         if (Tower != null){
             Debug.Log(Tower);
-            TowerShop.GetComponent<Shop>().CloseShopWindow();
+            shop.CloseShopWindow();
             // TODO - TowerUpgradeSystem.SetActive(true)
             return;
         } else {
-            TowerShop.GetComponent<Shop>().OpenShopWindow();
+            shop.OpenShopWindow();
         }
     }
     public void SetTower(GameObject Tower)
