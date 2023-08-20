acc8ce360be049f813a4c0648b20f653404dc0ce
Mohamad Amin Javan <aminjavan22478@gmail.com>
2023-08-20 14:06:50 +0200

    added move to random location near actual one

--- Assets/Scripts/MonoBehaviour/Enemies/EnemyMovement.cs
+++ Assets/Scripts/MonoBehaviour/Enemies/EnemyMovement.cs
@@ -2,30 +2,50 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private float MovementSpeed;
     private Transform target;
+    private float MovementSpeed;
+    private float offSet = 0.4f;
     private int wavepointIndex = 0;
+    private Vector3 randomTarget; // The random target position near the actual target
 
-    void Start() {
-        target = WaypointsScript.points[0];
+    private void Start()
+    {
+        target = WaypointsScript.points[wavepointIndex];
+        randomTarget = target.position;
         MovementSpeed = GetComponent<Enemy>().EnemyType.MovementSpeed;
     }
 
-    void Update() {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * MovementSpeed * Time.deltaTime, Space.World);
+    private void Update()
+    {
+        // Move towards random location near actual target
+        Vector3 dir = randomTarget - transform.position;
+        transform.Translate(dir.normalized * MovementSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, target.position) <= 0.4f){
+        // Calculate the random target position only if the enemy has reached the current target
+        if (Vector3.Distance(target.position, transform.position) <= offSet)
+        {
             GetNextWaypoint();
+            CalculateRandomTarget();
         }
     }
 
-    void GetNextWaypoint() {
-        if(wavepointIndex >= WaypointsScript.points.Length - 1) {
+    private void GetNextWaypoint()
+    {
+        if (wavepointIndex >= WaypointsScript.points.Length - 1)
+        {
             Destroy(gameObject);
-        } else {
+        }
+        else
+        {
             wavepointIndex++;
             target = WaypointsScript.points[wavepointIndex];
         }
+    }
+
+    private void CalculateRandomTarget()
+    {
+        // Generate a random offset within the specified range
+        Vector2 randomOffset = Random.insideUnitCircle * offSet;
+        randomTarget = target.position + new Vector3(randomOffset.x, randomOffset.y, 0);
     }
 }
