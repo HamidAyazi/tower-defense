98242f66ae06d761c1ae91a8c0e1351b675c0fcf
Hamid Ayazi <rubiker.hamid@gmail.com>
2023-08-31 15:33:37 +0330

    updated speed button image

--- Assets/SpeedManager.cs
+++ Assets/SpeedManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpeedManager : MonoBehaviour
 {
     private int Speed = 1;
+    [SerializeField] private Button speedButton;
+    [SerializeField] private Sprite speed1xImage;
+    [SerializeField] private Sprite speed2xImage;
+    [SerializeField] private Sprite speed3xImage;
 
     public void ChangeSpeed(){
-        Debug.Log("Hi");
         if(Speed == 1){
             X2Speed();
         } else if (Speed == 2){
@@ -19,15 +23,18 @@ public class SpeedManager : MonoBehaviour
     private void X2Speed() {
         Speed = 2;
         Time.timeScale = 2;
+        speedButton.image.sprite = speed2xImage;
     }
 
       private void X3Speed() {
         Speed = 3;
         Time.timeScale = 3;
+        speedButton.image.sprite = speed3xImage;
     }
 
       private void X1Speed() {
         Speed = 1;
         Time.timeScale = 1;
+        speedButton.image.sprite = speed1xImage;
     }
 }
