dbe057e6866df0ad5200234dcabe03b78e381c52
Mohamad Amin Javan <aminjavan22478@gmail.com>
2023-09-29 15:51:07 +0330

    build v0.5 stable

--- Assets/Scripts/ScriptableObject/GoalScriptableObject.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Goal", menuName = "ScriptableObjects/Goal")]
-public class GoalScriptableObject : ScriptableObject
-{
-    // Health amount of "Goal". It reduces when each "Enemy" hits Goal.
-    public int BaseHealthPoint;
-}
